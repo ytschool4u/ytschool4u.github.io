b26a7bb68df2f69e39287bfd77b68bc2c0e6d3ad
ytschool4u <136163491+ytschool4u@users.noreply.github.com>
2023-12-30 18:06:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -68,9 +68,9 @@ gsap.to("#about-p span",{
      start: `top bottom`,
       end: `bottom 70%`,
       scroller: `#main`,
-      scrub: .5,
+      scrub: .5,},
      stagger: .2,
-      color: `#fff`
+      color: `#fff`,
 })
 
 
@@ -118,6 +118,130 @@ scrollTrigger: {
 
 
 gsap.from("#icn-box .icn",{
+x:-500,
+  stagger: .7,
+  duration: 1,
+scrollTrigger: {
+ trigger: "#icn-box",
+ scroller: "#main",
+ start: "top 100%",
+ end: "top 40%",
+ // markers:true,
+ scrub: 2,
+}
+})
+
+// gsap.from(".about-p",{
+// opacity:0,
+//   stagger: .7,
+//   duration: 2,
+// scrollTrigger: {
+//  trigger: ".about-p",
+//  scroller: "#main",
+//  start: "top 80%",
+//  // end: "top 40%",
+//  // markers:true,
+//  scrub: 2,
+// }
+// })
+
+ 
+
+function loadCourse(){
+  // Your Firebase config here
+  const firebaseConfig = {
+   apiKey: "AIzaSyD-NSuXyWJtEb03OCk55uREmBepPnvFeek",
+   authDomain: "ytschool-ecb26.firebaseapp.com",
+   databaseURL: "https://ytschool-ecb26-default-rtdb.firebaseio.com",
+   projectId: "ytschool-ecb26",
+   storageBucket: "ytschool-ecb26.appspot.com",
+   messagingSenderId: "111162453586",
+   appId: "1:111162453586:web:9b72ec2c1aef899c22798b",
+   measurementId: "G-NYW5DTK5L1"
+  };
+ 
+  firebase.initializeApp(firebaseConfig);
+ 
+  var database = firebase.database();
+ 
+  // Reference to the 'course' node in the database
+  const coursesRef = database.ref('course');
+ 
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const courseId = urlParams.get('id');
+ 
+ 
+  const allContainer = document.getElementById('all');
+  const loadingText = document.getElementById('loadingText');
+ 
+ 
+  // Check if courseId has a value
+  if (courseId) {
+ 
+  } else {
+   
+  } 
+   // Show loading text while fetching data
+   loadingText.style.display = 'block';
+ 
+   // Fetch and display courses
+   coursesRef.once('value', function(snapshot) {
+      var courses = snapshot.val();
+ 
+      // Hide loading text
+      loadingText.style.display = 'none';
+ 
+      // Loop through each course and create HTML elements
+       for (const courseId in courses) {
+        const course = courses[courseId];
+        const courseElement = document.createElement('div');
+        courseElement.className = 'div-block';
+        courseElement.innerHTML = `
+                                           <img src="${course.image}" loading="lazy" alt="${course.name}" class="image-3">
+                                                       <p class="paragraph-2">${course.name}</p>
+                                                       <div class="div-block-2">
+                                                           <p class="paragraph-3"><i class="fa-solid fa-rupee-sign"></i> :  ${course.mode === 'free' ? 'Free' : `${course.dPrice}`}</p>
+                                                           <p class="paragraph-3"><i class="fa-regular fa-clock"></i> : 2 Hours/ Day</p>
+                                                       </div>
+                                                       <a href="${course.pLink1}" target="_blank" class="border-cta full-width w-button">Enroll Now</a>
+                                         `;
+      
+        allContainer.appendChild(courseElement);
+       }
+       });
+       }
+
+loadCourse();
+
+
+
+function cursorEffect(){
+ var page1Content = document.querySelector("#hero");
+ var cursor = document.querySelector("#cursor");
+ page1Content.addEventListener("mousemove", function(dets) {
+  gsap.to(cursor, {
+   x: dets.x,
+   y: dets.y,
+  })
+ });
+ 
+ page1Content.addEventListener("mouseenter", function() {
+  gsap.to(cursor, {
+   scale: 1,
+   opacity: 1
+  })
+ })
+ page1Content.addEventListener("mouseleave", function() {
+  gsap.to(cursor, {
+   scale: 0,
+   opacity: 0
+  })
+ })
+};
+cursorEffect();
+
+#icn-box .icn",{
 x:-500,
   stagger: .7,
   duration: 1,
