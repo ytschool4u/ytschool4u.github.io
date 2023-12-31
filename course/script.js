6fbcefedcc20f96acfa230dd6c085802a23fb15b
ytschool4u <136163491+ytschool4u@users.noreply.github.com>
2023-12-31 12:01:42 +0000

    gfgfgjhh

--- course/script.js
+++ course/script.js
@@ -1,4 +1,4 @@
- // Your Firebase config here
+// Your Firebase config here
  const firebaseConfig = {
   apiKey: "AIzaSyD-NSuXyWJtEb03OCk55uREmBepPnvFeek",
   authDomain: "ytschool-ecb26.firebaseapp.com",
@@ -9,58 +9,89 @@
   appId: "1:111162453586:web:9b72ec2c1aef899c22798b",
   measurementId: "G-NYW5DTK5L1"
  };
-
+ 
  firebase.initializeApp(firebaseConfig);
-
+ 
  var database = firebase.database();
-
+ 
  // Reference to the 'course' node in the database
  const coursesRef = database.ref('course');
-
+ 
  const queryString = window.location.search;
  const urlParams = new URLSearchParams(queryString);
  const courseId = urlParams.get('id');
-
-
+ 
  const cMainContainer = document.getElementById('c-main');
  const allContainer = document.getElementById('all');
  const loadingText = document.getElementById('loadingText');
+ 
+ // Function to load data based on ID
+ function loadDataById(courseId) {
+  coursesRef.orderByChild('id').equalTo(courseId).once('value', (snapshot) => {
+   // The snapshot.val() contains the data
+   const courseData = snapshot.val();
+ 
+   // Process and display the data, e.g., update DOM elements
+   if (courseData) {
+    const course = courseData[Object.keys(courseData)[0]]; // Assuming one course per ID
 
-
+ 
+    // Example: Update your DOM elements with details of the selected course
+    document.querySelector('.image-3').src = course.image;
+    document.querySelector('#c-name').innerText = course.name;
+    document.querySelector('#c-price').innerHTML = `<i class="fa-solid fa-rupee-sign"></i> : ${course.mode === 'free' ? 'Free' : `${course.dPrice}`}`;
+    document.querySelector('#des').innerHTML = `${course.description}`;
+    document.querySelector('#enroll').innerHTML= `<a href='${course.pLink1}'>Enroll Now<\a>`;
+   document.querySelector('#c-title').innerText = course.name;
+    // ... Update other elements as needed
+   } else {
+    console.log('Course not found');
+   }
+ 
+   // Hide loading text
+   loadingText.style.display = 'none';
+  });
+ }
+ 
  // Check if courseId has a value
  if (courseId) {
   // courseId has a value, hide 'all-course', show 'c-main'
   cMainContainer.style.display = 'block';
+ 
+ 
+ 
+  // Fetch and display data for the specific course ID
+  loadDataById(courseId);
  } else {
   // courseId is empty, hide 'c-main'
-  cMainContainer.style.display = 'none';
-
+  cMainContainer.style.display = 'none';}
+ 
   // Show loading text while fetching data
   loadingText.style.display = 'block';
-
+ 
   // Fetch and display courses
   coursesRef.once('value', function(snapshot) {
    var courses = snapshot.val();
-
+ 
    // Hide loading text
    loadingText.style.display = 'none';
-
+ 
    // Loop through each course and create HTML elements
    for (const courseId in courses) {
     const course = courses[courseId];
     const courseElement = document.createElement('div');
     courseElement.className = 'div-block';
     courseElement.innerHTML = `
-                           <img src="${course.image}" loading="lazy" alt="${course.name}" class="image-3">
-                           <p class="paragraph-2">${course.name}</p>
-                           <div class="div-block-2">
-                               <p class="paragraph-3"><i class="fa-solid fa-rupee-sign"></i> :  ${course.mode === 'free' ? 'Free' : `${course.dPrice}`}</p>
-                               <p class="paragraph-3"><i class="fa-regular fa-clock"></i> : 2 Hours/ Day</p>
-                           </div>
-                           <a href="${course.pLink1}" target="_blank" class="border-cta full-width w-button">Enroll Now</a>
-                       `;
-
+         <img src="${course.image}" loading="lazy" alt="${course.name}" class="image-3">
+         <p class="paragraph-2">${course.name}</p>
+         <div class="div-block-2">
+           <p class="paragraph-3"><i class="fa-solid fa-rupee-sign"></i> : ${course.mode === 'free' ? 'Free' : `${course.dPrice}`}</p>
+           <p class="paragraph-3"><i class="fa-regular fa-clock"></i> : 2 Hours/ Day</p>
+         </div>
+         <a href="/?id=${course.id}" target="_blank" class="border-cta full-width w-button">Enroll Now</a>
+       `;
+ 
     allContainer.appendChild(courseElement);
    }
   });
- }
+ 
--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-function loco(){
-    gsap.registerPlugin(ScrollTrigger);
+function loco() {
+ gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+ // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-const locoScroll = new LocomotiveScroll({
+ const locoScroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+ });
+ // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+ locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#main", {
+ // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ ScrollTrigger.scrollerProxy("#main", {
   scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+   return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
   getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+   return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
   },
   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
   pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-});
+ });
 
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
+ // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ ScrollTrigger.refresh();
 }
 loco()
 
@@ -36,63 +36,64 @@ loco()
 
 var clutter = "";
 
-document.querySelector("#page2 h1").textContent.split("").forEach(function(dets){
-    clutter += `<span>${dets}</span>`
+document.querySelector("#page2 h1").textContent.split("").forEach(function(dets) {
+ clutter += `<span>${dets}</span>`
 
-    document.querySelector("#page2 h1").innerHTML = clutter;
+ document.querySelector("#page2 h1").innerHTML = clutter;
 })
 
-gsap.to("#page2 h1 span",{
-    scrollTrigger: {
-      trigger: "#page2 h1",
-     start: `top bottom`,
-      end: `bottom 70%`,
-      scroller: `#main`,
-      scrub: .5,
-     },
-     stagger: .2,
-      color: `#fff`
+gsap.to("#page2 h1 span", {
+ scrollTrigger: {
+  trigger: "#page2 h1",
+  start: `top bottom`,
+  end: `bottom 70%`,
+  scroller: `#main`,
+  scrub: .5,
+ },
+ stagger: .2,
+ color: `#fff`
 })
 
 var clutter = "";
 
-document.querySelector("#about-p").textContent.split("").forEach(function(dets){
-    clutter += `<span>${dets}</span>`
+document.querySelector("#about-p").textContent.split("").forEach(function(dets) {
+ clutter += `<span>${dets}</span>`
 
-    document.querySelector("#about-p").innerHTML = clutter;
+ document.querySelector("#about-p").innerHTML = clutter;
 })
 
-gsap.to("#about-p span",{
-    scrollTrigger: {
-      trigger: "#about-p",
-     start: `top bottom`,
-      end: `bottom 70%`,
-      scroller: `#main`,
-      scrub: .5,},
-     stagger: .2,
-      color: `#fff`,
+gsap.to("#about-p span", {
+ scrollTrigger: {
+  trigger: "#about-p",
+  start: `top bottom`,
+  end: `bottom 70%`,
+  scroller: `#main`,
+  scrub: .5,
+ },
+ stagger: .2,
+ color: `#fff`,
 })
 
 
 
 
- gsap.from("#left *",{
-x:-200,
-duration:1,
-  stagger: .2,
+gsap.from("#left *", {
+ x: -200,
+ duration: 1,
+ stagger: .2,
 })
-gsap.from("#right *",{
+gsap.from("#right *", {
  x: 200,
  duration: 2,
-  stagger: .2,
- 
+ stagger: .2,
+
 
 })
-gsap.from("header *",{
+gsap.from("header *", {
  y: -100,
  duration: .5,
-  stagger: .2,
- 
+ stagger: .2,
+
 })
 
 
@@ -100,35 +101,35 @@ gsap.from("header *",{
 
 
 
-gsap.from(".hero-2 > img",{
-x: -200,
-  stagger: 1,
-  duration: 1,
-scrollTrigger: {
- trigger: ".hero-2",
- scroller: "#main",
- start: "top 80%",
- end: "top 20%",
- // markers:true,
- scrub: 2,
-}
+gsap.from(".hero-2 > img", {
+ x: -200,
+ stagger: 1,
+ duration: 1,
+ scrollTrigger: {
+  trigger: ".hero-2",
+  scroller: "#main",
+  start: "top 80%",
+  end: "top 20%",
+  // markers:true,
+  scrub: 2,
+ }
 })
 
 
 
 
-gsap.from("#icn-box .icn",{
-x:-500,
-  stagger: .7,
-  duration: 1,
-scrollTrigger: {
- trigger: "#icn-box",
- scroller: "#main",
- start: "top 100%",
- end: "top 40%",
- // markers:true,
- scrub: 2,
-}
+gsap.from("#icn-box .icn", {
+ x: -500,
+ stagger: .7,
+ duration: 1,
+ scrollTrigger: {
+  trigger: "#icn-box",
+  scroller: "#main",
+  start: "top 100%",
+  end: "top 40%",
+  // markers:true,
+  scrub: 2,
+ }
 })
 
 // gsap.from(".about-p",{
@@ -145,78 +146,78 @@ scrollTrigger: {
 // }
 // })
 
- 
 
-function loadCourse(){
-  // Your Firebase config here
-  const firebaseConfig = {
-   apiKey: "AIzaSyD-NSuXyWJtEb03OCk55uREmBepPnvFeek",
-   authDomain: "ytschool-ecb26.firebaseapp.com",
-   databaseURL: "https://ytschool-ecb26-default-rtdb.firebaseio.com",
-   projectId: "ytschool-ecb26",
-   storageBucket: "ytschool-ecb26.appspot.com",
-   messagingSenderId: "111162453586",
-   appId: "1:111162453586:web:9b72ec2c1aef899c22798b",
-   measurementId: "G-NYW5DTK5L1"
-  };
- 
-  firebase.initializeApp(firebaseConfig);
- 
-  var database = firebase.database();
- 
-  // Reference to the 'course' node in the database
-  const coursesRef = database.ref('course');
- 
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const courseId = urlParams.get('id');
- 
- 
-  const allContainer = document.getElementById('all');
-  const loadingText = document.getElementById('loadingText');
- 
- 
-  // Check if courseId has a value
-  if (courseId) {
- 
-  } else {
-   
-  } 
-   // Show loading text while fetching data
-   loadingText.style.display = 'block';
- 
-   // Fetch and display courses
-   coursesRef.once('value', function(snapshot) {
-      var courses = snapshot.val();
- 
-      // Hide loading text
-      loadingText.style.display = 'none';
- 
-      // Loop through each course and create HTML elements
-       for (const courseId in courses) {
-        const course = courses[courseId];
-        const courseElement = document.createElement('div');
-        courseElement.className = 'div-block';
-        courseElement.innerHTML = `
+
+function loadCourse() {
+ // Your Firebase config here
+ const firebaseConfig = {
+  apiKey: "AIzaSyD-NSuXyWJtEb03OCk55uREmBepPnvFeek",
+  authDomain: "ytschool-ecb26.firebaseapp.com",
+  databaseURL: "https://ytschool-ecb26-default-rtdb.firebaseio.com",
+  projectId: "ytschool-ecb26",
+  storageBucket: "ytschool-ecb26.appspot.com",
+  messagingSenderId: "111162453586",
+  appId: "1:111162453586:web:9b72ec2c1aef899c22798b",
+  measurementId: "G-NYW5DTK5L1"
+ };
+
+ firebase.initializeApp(firebaseConfig);
+
+ var database = firebase.database();
+
+ // Reference to the 'course' node in the database
+ const coursesRef = database.ref('course');
+
+ const queryString = window.location.search;
+ const urlParams = new URLSearchParams(queryString);
+ const courseId = urlParams.get('id');
+
+
+ const allContainer = document.getElementById('all');
+ const loadingText = document.getElementById('loadingText');
+
+
+ // Check if courseId has a value
+ if (courseId) {
+
+ } else {
+
+ }
+ // Show loading text while fetching data
+ loadingText.style.display = 'block';
+
+ // Fetch and display courses
+ coursesRef.once('value', function(snapshot) {
+  var courses = snapshot.val();
+
+  // Hide loading text
+  loadingText.style.display = 'none';
+
+  // Loop through each course and create HTML elements
+  for (const courseId in courses) {
+   const course = courses[courseId];
+   const courseElement = document.createElement('div');
+   courseElement.className = 'div-block';
+   courseElement.innerHTML = `
                                            <img src="${course.image}" loading="lazy" alt="${course.name}" class="image-3">
                                                        <p class="paragraph-2">${course.name}</p>
                                                        <div class="div-block-2">
                                                            <p class="paragraph-3"><i class="fa-solid fa-rupee-sign"></i> :  ${course.mode === 'free' ? 'Free' : `${course.dPrice}`}</p>
                                                            <p class="paragraph-3"><i class="fa-regular fa-clock"></i> : 2 Hours/ Day</p>
                                                        </div>
-                                                       <a href="${course.pLink1}" target="_blank" class="border-cta full-width w-button">Enroll Now</a>
+                                                       <a href="/course/?id=${course.id}" target="_blank" class="border-cta full-width w-button">Enroll Now</a>
                                          `;
-      
-        allContainer.appendChild(courseElement);
-       }
-       });
-       }
+
+   allContainer.appendChild(courseElement);
+  }
+ });
+}
 
 loadCourse();
 
 
 
-function cursorEffect(){
+function cursorEffect() {
  var page1Content = document.querySelector("#hero");
  var cursor = document.querySelector("#cursor");
  page1Content.addEventListener("mousemove", function(dets) {
@@ -225,7 +226,7 @@ function cursorEffect(){
    y: dets.y,
   })
  });
- 
+
  page1Content.addEventListener("mouseenter", function() {
   gsap.to(cursor, {
    scale: 1,
@@ -240,4 +241,3 @@ function cursorEffect(){
  })
 };
 cursorEffect();
-
